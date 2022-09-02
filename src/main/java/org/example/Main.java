4b2df8128b2e061b0d83f91893fe8e7b0f0d8ec3
king0404 <xhakxh147@handong.ac.kr>
2022-09-02 10:32:25 +0900

    thrid commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(" 안녕~!!!! ");
+        System.out.println(" 안녕~! !!! ");
     }
 }
